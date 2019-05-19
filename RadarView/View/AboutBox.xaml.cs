4e750e315418aba0b85a63f47f24251c1d44adb2
Martin Honěk <445350@muni.cz>
2019-05-20 01:57:44 +0200

    -pridany odkaz na repozitar

--- RadarView/View/AboutBox.xaml.cs
+++ RadarView/View/AboutBox.xaml.cs
@@ -11,5 +11,10 @@ namespace RadarView.View
         {
             this.InitializeComponent();
         }
-    }
+
+		private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+		{
+			System.Diagnostics.Process.Start(e.Uri.ToString());
+		}
+	}
 }
